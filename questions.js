2bd7e2d9eb53e20d78b181d932297354de49c132
Darshan110801 <73900965+Darshan110801@users.noreply.github.com>
2021-10-23 13:12:13 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,83 +1,16 @@
 
-let response = fetch("https://quizboot.herokuapp.com/random_upto_max/5", {
-  mode: "no-cors",
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Credentials": true,
-  },
-})
-  .then((res) => res.json())
-  .then((resp) => {
-    console.log(resp);
-  });
-questions = [
-    {
-    numb: 1,
-    question: "What new element was discovered on the Moon?",
-    answer: "Armalcolite",
-    options: [
-      "Ferrite",
-      "Selenium",
-      "Armalcolite",
-      "Malcoarmite"
-    ]
-  },
-    {
-    numb: 2,
-    question: "What does ISRO stand for?",
-    answer: " Indian Space Research Organisation",
-    options: [
-      " Indiana Space Requisite Organisation",
-      " Indian Station Research Organisation",
-      " Indian Scholar Research Organisation",
-      " Indian Space Research Organisation"
-    ]
-  },
-    {
-    numb: 3,
-    question: "Which was the first satellite launched by ISRO?",
-    answer: "Kalpana-1",
-    options: [
-      "Kalpana-1",
-      "Bhaskara",
-      "Aryabhata",
-      "Chandrayaan"
-    ]
-  },
-    {
-    numb: 4,
-    question: "In which place Dr Vikram Sarabhai Space Centre is located?",
-    answer: "Sriharikota",
-    options: [
-      "Thiruvananthapuram",
-      "Bangalore",
-      "Sriharikota",
-      "None Of these"
-    ]
-  },
-    {
-    numb: 5,
-    question: "Who is the father of modern space science in India?",
-    answer: "Vikram Sarabhai",
-    options: [
-      "Satish Dhawan",
-      "Vikram Sarabhai",
-      "Dr. Apj abdul kalam",
-      "C V Raman"
-    ]
-  },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
-];
+function fetcher()  {fetch("https://quizboot.herokuapp.com/random_upto_max/10")
+  .then(response => response.json())
+  .then(data => { questions = data; }).then(()=>{
+    let new_questions = [];
+    let count =1;
+    console.log(questions);
+    for (question_i of questions){
+       new_question = {numb:count,question : question_i.statement,options:[question_i.option1,question_i.option2,question_i.option3,question_i.option4]}
+       new_question.answer = new_question.options[question_i.correct_ans-1];
+       new_questions.push(new_question)
+    }
+    console.log(new_questions)
+    questions = new_questions
+  })}
+fetcher();
